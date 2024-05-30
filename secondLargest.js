da88a655b30e398c3c3a140b074d0236d9ee8d42
adithya <adithya.upadhyaya007@gmail.com>
2024-05-30 21:11:29 +0530

    commit

---
+++ secondLargest.js
@@ -0,0 +1,47 @@
+// Complete the getSecondLargest function in the editor below.
+
+// getSecondLargest has the following parameters:
+
+// int nums[n]: an array of integers
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+  let new_num = [...new Set(nums)];
+  let sorted = new_num.sort((a,b)=>b-a)
+return sorted[1]
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
